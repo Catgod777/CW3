fb09dc6367df31058d3bbb7887ef83dbb409c3d7
Catgod777 <133408572+Catgod777@users.noreply.github.com>
2023-05-12 19:21:44 +0100

    Add files via upload

---
+++ survis_0.1.0_20151022/src/data/generated/available_img.js
@@ -0,0 +1,1 @@
+define({ availableImg: ["Chidepatil2020","Park2011","Hinarejos2020","Chukleang2022"]});
---
+++ survis_0.1.0_20151022/src/data/generated/bib.js
@@ -0,0 +1,56 @@
+﻿define({ entries : {
+    "Chidepatil2020": {
+        "abstract": "Distributed ledger technologies are emerging as important tools that can positively impact almost all major industries and activities in our society. They are increasingly used in various supply (value) chain applications including financial, governance, regulations, health, logistics, and other industrial sectors. In this letter, we are presenting various ongoing work related to different distributed ledger technologies and their applications in postal sector. In particular, we will highlight some of the innovations in the inclusive financial services for the underbanked and unbanked populations using distributed ledger technologies.",
+        "author": "Aditya Chidepatil and Krishnaswamy Sankaran",
+        "doi": "10.24840/2183-0606_008.003_0003",
+        "issn": "21830606",
+        "issue": "3",
+        "journal": "Journal of Innovation Management",
+        "keywords": "Blockchain,DLT,Distributed ledger technology,Financial services innovation,Inclusive financial services,Postal logistics,Supply chain,Unbanked,Underbanked,Value chain",
+        "month": "12",
+        "pages": "20-27",
+        "publisher": "Universidade do Porto - Faculdade de Engenharia",
+        "title": "Distributed ledger technologies for inclusive postal financial services: Blockchain and more",
+        "type": "article",
+        "volume": "8",
+        "year": "2020"
+    },
+    "Chukleang2022": {
+        "abstract": "Home delivery (B2C) experienced rapid growth during the COVID-19 pandemic, especially food delivery. Nonrepudiation is a problem in security and affects delivery. Blockchain technology is a new technology that addresses this issue. This paper proposes preventing nonrepudiation in home delivery through blockchain technology. We also design a data structure and smart contract for distributed application. In addition, we evaluate the performance of the proposed framework in terms of transaction fees and response times. We experimented on the blockchain emulator, stored data in RDBMS, and implemented a client with a mobile application. The data in the smart contract were directly impacted by the gas used and the response time. Primarily, the function processes the large data size and involves high transaction fees and long response times. The gas used accounts for 9061 times of data length, while the response time accounts for 2.84 times of data length. Finally, we propose a security policy for the proposed framework.",
+        "author": "Tirajet Chukleang and Chanankorn Jandaeng",
+        "doi": "10.3390/informatics9030070",
+        "issn": "22279709",
+        "issue": "3",
+        "journal": "Informatics",
+        "keywords": "blockchain application,home delivery,logistics,smart contract",
+        "month": "9",
+        "publisher": "MDPI",
+        "title": "Security Enhancement in Smart Logistics with Blockchain Technology: A Home Delivery Use Case",
+        "type": "article",
+        "volume": "9",
+        "year": "2022"
+    },
+    "Hinarejos2020": {
+        "abstract": "In this article, we present the first solution for multi-party certified email that achieves fairness and confidentiality without the intervention of a TTP but with blockchain support when problems arise. Our solution preserves the confidentiality of the mail exchanged between the sender and the recipients even when the blockchain is involved. We provide a proof of concept implementation based on Ethereum smart contracts that shows it is feasible from a practical point of view. Because the functions of a smart contract are only executed on the blockchain in the case of conflict, costs are minimised compared to solutions that execute the functions on the blockchain in all cases (on-chain solutions). Moreover, the solution can be integrated into the existing email infrastructure.",
+        "author": "M. Francisca Hinarejos and Josep Lluis Ferrer-Gomila",
+        "doi": "10.1109/ACCESS.2020.2998679",
+        "issn": "21693536",
+        "journal": "IEEE Access",
+        "keywords": "Blockchain,certified delivery,certified email,electronic notifications,email security,fair exchange,nonrepudiation",
+        "pages": "102997-103006",
+        "publisher": "Institute of Electrical and Electronics Engineers Inc.",
+        "title": "A Solution for Secure Multi-Party Certified Electronic Mail Using Blockchain",
+        "type": "article",
+        "volume": "8",
+        "year": "2020"
+    },
+    "Park2011": {
+        "abstract": "This paper presents work load factors and management system of postal delivery operation. The proposed work load factors and management for postal delivery operation which consists of delivery sequence sorting, moving, mail delivery, and additional work for mail delivery is based on the number of delivery mail, a kind of mail, moving and delivery environments such as APT, Villa, house area, school/hospital/public build-ing/commercial& business area, and complex building & officetel area. There is also system architecture and user interface for work load management of postal delivery operation which is based on the proposed model in this paper. The management system will be adapted for more relational and efficient work load management of postal delivery operation.",
+        "author": "Jeong-Hyun Park and Jong-Heung Park",
+        "journal": "The 4th International Conference on Interaction Sciences",
+        "keywords": "Postal Delivery Operation,Postal Logistics,Work Load Measurement",
+        "title": "System Architecture for Work Load Management of Postal Delivery Operation",
+        "type": "report",
+        "year": "2011"
+    }
+}});
